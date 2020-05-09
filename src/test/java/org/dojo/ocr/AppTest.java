6676073b761733f347bbcd823a79142d0518f12f
Leo Rodrigues <leorodriguesrj@gmail.com>
2020-05-09 16:38:44 -0300

    Basic project structure

---
+++ src/test/java/org/dojo/ocr/AppTest.java
@@ -0,0 +1,17 @@
+package org.dojo.ocr;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+
+    @Test
+    void testApp() {
+        int i = 1;
+        assertTrue(i == 1);
+    }
+}
